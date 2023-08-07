b1e2a26f69b45f826b779bae2415615df81de1c3
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-08-07 06:24:33 +0300

    trying to apply the task using C#

---
+++ Sessions/session14/SessionTask/IWorkflowContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SessionTask
+{
+    internal interface IWorkflowContext
+    {
+        Guid PrimaryEntityId { get; }
+        string PrimaryEntityName { get; }
+        object UserId { get; }
+    }
+}
