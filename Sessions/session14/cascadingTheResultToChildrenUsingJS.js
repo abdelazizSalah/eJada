a1a774bd9a449dcd1e1b7fbe3e780e8a1c2aa86c
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-08-08 10:29:50 +0300

    finshing the js code for the cascading task

--- Sessions/session14/cascadingTheResultToChildrenUsingJS.js
+++ Sessions/session14/cascadingTheResultToChildrenUsingJS.js
@@ -1,25 +1,61 @@
-function CascadeRelatedRecords(executionContext) {
-    debugger;
+// function CascadeRelatedRecords(executionContext) {
+//     debugger;
 
-    // getting the form context 
+//     // getting the form context 
+//     var formContext = executionContext.getFormContext();
+
+//     // get the fax of that account
+//     var fax = formContext.getAttribute("fax").getValue();
+
+//     // iterate over all the contacts which are related to that account
+//     // and set that fax to their fax 
+//     // formContext.getAttribute("contact_customer_accounts").getValue().forEach(function (contact) {
+//     //     contact.fax = fax;
+//     // });
+
+
+//     // iterate over all the activities which are related to the case and set the description to the case description
+//     formContext.getAttribute("Incident_Phonecalls").getValue().forEach(function (activity) {
+//         activity.description = formContext.getAttribute("description").getValue();
+//         // save the changes
+//         formContext.getAttribute("contact_customer_accounts").setValue(contact);
+//     }
+//     );
+
+// }
+
+
+function updateFax(executionContext) {
+    debugger;
+    // get the execution context from the form 
     var formContext = executionContext.getFormContext();
 
-    // get the fax of that account
-    var fax = formContext.getAttribute("fax").getValue();
+    // now lets get the id of the company   
+    var accountId = formContext.data.entity.getId().replace("{", "").replace("}", "");
 
-    // iterate over all the contacts which are related to that account
-    // and set that fax to their fax 
-    // formContext.getAttribute("contact_customer_accounts").getValue().forEach(function (contact) {
-    //     contact.fax = fax;
-    // });
+    // try to access the fax 
+    var accountFax = formContext.getAttribute("fax").getValue();
 
+    // now we need to create a  map for the data 
+    var data = { fax: accountFax };
 
-    // iterate over all the activities which are related to the case and set the description to the case description
-    formContext.getAttribute("Incident_Phonecalls").getValue().forEach(function (activity) {
-        activity.description = formContext.getAttribute("description").getValue();
-        // save the changes
-        formContext.getAttribute("contact_customer_accounts").setValue(contact);
-    }
+    // lets start using the webAPI. 
+    Xrm.WebApi.retrieveMultipleRecords('contact', "?$select=contactid&$filter=_parentcustomerid_value eq " + accountId).then(
+        function success(result) {
+            for (var i = 0; i < result.entities.length; i++) {
+                // now we are iterating over all the existing contacts. 
+                var contact = result.entities[i];
+
+                // we have to send a new request to update its fax. 
+                Xrm.WebApi.updateRecord("contact", contact.contactid, data).then(
+                    function success(result) {
+                        console.log("Fax updated successfully");
+                    },
+                    function (error) {
+                        console.log(error.message);
+                    }
+                );
+            }
+        }
     );
-
 }
